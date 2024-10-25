5c52d481f6f3c0518ff57dcb1ded59fadbc2c842
7DARKNESS7 <jorika911@gmail.com>
2024-10-25 18:17:08 +0400

    first commit

---
+++ js/home.js
@@ -0,0 +1,31 @@
+let favoriteRooms = document.getElementById("favoriteRoomsList");
+
+function showFavoriteCards(item) {
+  return `
+              <div class="roomCard">
+            <img
+              src="${item.images[0].source}"
+              alt=""
+            />
+            <div class="roomInfo">
+              <div>
+                <span class="roomName">${item.name}</span>
+                <span class="roomPrice">€ ${item.pricePerNight}</span>
+              </div>
+              <button>BOOK NOW</button>
+            </div>
+          </div>
+    `;
+}
+
+function getFavoriteRooms() {
+  fetch("https://hotelbooking.stepprojects.ge/api/Rooms/GetAll")
+    .then((response) => response.json())
+    .then((data) => {
+      let limitedRooms = data.slice(0, 6);
+      limitedRooms.forEach((item) => {
+        favoriteRooms.innerHTML += showFavoriteCards(item);
+      });
+    });
+}
+getFavoriteRooms();
